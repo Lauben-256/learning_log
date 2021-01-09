7df4fef5abe7dace6d6e47ee0b0e5cbe7e406a83
Tumusiime Lauben Junior <tumusiimelauben9@gmail.com>
2021-01-09 19:03:55 +0800

    Updated Views

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
+
+# Custom Functions
+""" Check if user associated with a topic matches the currently logged-in user """
+def check_topic_owner(request, id):
+    topic = Topic.objects.get(pk = id)
+    if topic.owner != request.user:
+        raise Http404
+
 def index(request):
     """ The homepage for Learning  Log """
     return render(request, 'learning_logs/index.html')
@@ -25,8 +34,11 @@ def topic(request, id):
     """ Show a single topic and all its entries. """
     topic = Topic.objects.get(pk = id)
     # Make sure the topic belongs to the current user.
+    """
     if topic.owner != request.user:
         raise Http404
+    """
+    check_topic_owner(request, id)
 
     entries = topic.entry_set.order_by('-date_added')
     context = {
@@ -57,7 +69,9 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """ Add a new entry for a particular topic """
     topic = Topic.objects.get(pk = topic_id)
-    
+    # Check if the topic to add an entry belongs to the currently logged in user. 
+    check_topic_owner(request, topic_id)
+
     if request.method != 'POST':
         # No data submitted, create a blank form. 
         form = EntryForm()
@@ -78,8 +92,11 @@ def edit_entry(request, entry_id):
     # Edit an existing entry
     entry = Entry.objects.get(pk = entry_id)
     topic = entry.topic
+    """
     if topic.owner != request.user:
         raise Http404
+    """
+    check_topic_owner(request, entry_id)
 
     if request.method != 'POST':
         # Initital request; pre-fill form with the current entry.
