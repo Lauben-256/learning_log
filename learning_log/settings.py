a18b6ea7980d43baa78c960f4d0ca3b2d89de46c
Tumusiime Lauben Junior <tumusiimelauben9@gmail.com>
2021-01-11 23:55:31 +0800

    Changed database engine to mysql

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -142,7 +142,7 @@ BOOTSTRAP4 = {
 if os.getcwd() == '/app':
     import dj_database_url
     DATABASES = {
-        'default': dj_database_url.config(default='postgres://localhost')
+        'default': dj_database_url.config(default='mysql://localhost')
     }
 
     # Honor the 'X-Forwarded-Proto' header for request.is_secure().
